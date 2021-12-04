0141766fabbba69728cb0b5c425f11ed4938c4bd
Tiago Hennig <tiago.hennig@hotmail.com>
2021-12-04 17:58:44 -0300

    projeto-blackjack-ok

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,24 +1,28 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-   
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
-
 console.log("BOAS VINDAS AO JOGO DE BLACKJACK!")
 
-if (confirm(prompt("Quer iniciar uma nova rodada?"))) {
+if (confirm("Quer iniciar uma nova rodada?")) {
+   const carta1 = comprarCarta()
+   const carta2 = comprarCarta()
+   const carta1pc = comprarCarta()
+   const carta2pc = comprarCarta()
+   
+   const pontosUsuario = (carta1.valor) + (carta2.valor)
+   const pontosPc = (carta1pc.valor) + (carta2pc.valor)
+
+   console.log("Usuário - cartas: " +(carta1.texto) + " " +(carta2.texto) + " - pontuação " + pontosUsuario)
+   console.log("Computador - cartas: " +(carta1pc.texto) + " " +(carta2pc.texto) + " - pontuação " + pontosPc)
+
+   if (pontosUsuario === pontosPc) {
+      console.log("Empate!")
+   } else if (pontosUsuario > pontosPc) {
+      console.log("O usuário ganhou!")
+   } else {
+      console.log("O computador ganhou!")
+   }
 
 } else {
    console.log("O jogo acabou.")
 }
-
 
 
 
