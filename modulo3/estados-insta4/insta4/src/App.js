ef02b61f23179c554e1dce3537e145fe8568b3dc
Tiago Hennig <tiago.hennig@hotmail.com>
2022-02-05 18:46:29 -0300

    .

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -18,6 +18,18 @@ class App extends React.Component {
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
         />
+
+        <Post
+          nomeUsuario={'alvaro'}
+          fotoUsuario={'https://picsum.photos/50/51'}
+          fotoPost={'https://picsum.photos/200/151'}
+        />
+
+        <Post
+          nomeUsuario={'james'}
+          fotoUsuario={'https://picsum.photos/50/52'}
+          fotoPost={'https://picsum.photos/200/152'}
+        />
       </MainContainer>
     );
   }
