84108e3d956a5553471b85bd11c0cd6e412ed734
Tiago Hennig <tiago.hennig@hotmail.com>
2022-03-02 16:37:12 -0300

    melhoras

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -2,6 +2,36 @@ import './App.css';
 import styled from 'styled-components'
 import React from 'react';
 import Mensagem from './components/Mensagem';
+import Background from './img/background.png'
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+
+`
+
+const Digitacao = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Msgs = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+`
+
+const Box = styled.div`
+  display: flex;
+  height: 91.5vh;
+  border: solid black 1px;
+  width: 393px;
+  justify-content: center;
+  align-self: center;
+  margin-top: 20px;
+  margin-bottom: 2px;
+  background-image: url(${Background});
+`
 
 
 class App extends React.Component {
@@ -21,24 +51,26 @@ handleInputMensagemChange = (event) => {
 }
 
 onClickEnviar = () => {
-  this.setState({mensagens: [...this.state.mensagens,{usuario: this.state.usuario, mensagem: this.state.mensagem}] })
+  this.setState({mensagens: [{usuario: this.state.usuario, mensagem: this.state.mensagem},...this.state.mensagens] , mensagem: '', usuario: '' })
 }
-
 
   render() {
   return (
-    <div>
-        <div>
-          {this.state.mensagens.map(item => <Mensagem usuario={item.usuario} mensagem ={item.mensagem}/>)}
-        </div>
+    <Page>
 
-        <div>
+        <Box>
+          <Msgs>
+            {this.state.mensagens.map(item => <Mensagem usuario={item.usuario} mensagem ={item.mensagem}/>)}
+          </Msgs>
+        </Box>
+
+        <Digitacao>
           <input placeholder='Usuário' type="text" value={this.state.usuario} onChange={this.handleInputUsuarioChange}/>
           <input placeholder='Mensagem' type="text" value={this.state.mensagem} onChange={this.handleInputMensagemChange} />
-          <button onClick={this.onClickEnviar}> Enviar </button>
-        </div>
+          <button type="reset" onClick={this.onClickEnviar}> Enviar </button>
+        </Digitacao>
 
-    </div>
+    </Page>
   );
 }
 }
