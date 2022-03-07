e85ee627e214137ecbac149b1b18f3dfe8684ade
Tiago Hennig <tiago.hennig@hotmail.com>
2022-03-06 21:14:39 -0300

    iniciando a parte de renderização de playlists

--- modulo3/projeto-labefy/labefy/src/App.js
+++ modulo3/projeto-labefy/labefy/src/App.js
@@ -3,103 +3,32 @@ import './App.css';
 import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import { DivPage, Div, Header, Main, Coluna, 
+  CriarPlaylist, ListaDePlaylists, AdicionarMusicas, 
+DetalheDaPlaylist, DivPlaylistBotao, BotaoApagar } from './components/style';
 
-const Header = styled.header`
-  display: flex;
-  justify-content: center;
-  height: 90px;
-  background-color: skyblue;
-  font-size: x-large;
-  font-weight: bold;
-  margin: 10px;
-  border-radius: 10px;
-`
-
-const Main = styled.div`
-display: flex;
-background-color: white;
-`
-
-const Coluna = styled.div`
-display: flex;
-flex-direction: column;
-width: 300px;
-background-color: white;
-`
-
-const CriarPlaylist = styled.div`
-display: flex;
-flex-direction: column;
-margin: 10px;
-background-color: aliceblue;
-text-align: center;
-min-height: 100px;
-border-radius: 10px;
-padding-bottom: 20px;
-
-  div {
-    display: flex;
-  flex-direction: column;
-  width: 160px;
-  place-self: center;
-  }
-
-  div > input {
-    margin: 3px;
-  }
-
-  div > button {
-    margin: 3px;
-  }
-`
-
-
-const ListaDePlaylists = styled.div`
-display: flex;
-flex-direction: column;
-margin: 10px;
-background-color: aliceblue;
-text-align: center;
-min-height: 250px;
-border-radius: 10px;
-
-  div {
-    display: flex;
-    flex-direction: column;
-}
-`
-
-const AdicionarMusicas = styled.div`
-display: flex;
-flex-direction: column;
-margin: 10px;
-background-color: aliceblue;
-text-align: center;
-height: 150px;
-border-radius: 10px;
-width: 300px;
-`
-
-const DetalheDaPlaylist = styled.div`
-display: flex;
-flex-direction: column;
-margin: 10px;
-background-color: aliceblue;
-text-align: center;
-height: 150px;
-border-radius: 10px;
-width: 300px;
-`
 
 class App extends React.Component {
   state ={
     playlists: [],
-    inputCP: ''
+    inputCP: '',
+    inputPlaylist: '',
+    inputTrack: '',
+    inputArtist: '',
+    inputURL: '',
+    faixasDaPlaylist: []
   }
 
   componentDidMount() {
     this.getAllPlaylists()
+    this.getPlaylistTracks()
   }
+
+  componentDidUpdate() {
+    this.getPlaylistTracks()
+  }
+
+
 
 
   getAllPlaylists = () => {
@@ -115,10 +44,25 @@ class App extends React.Component {
   }
 
 
-  handleInputCP = (event) => {
-    this.setState({ inputCP: event.target.value})
-    console.log(this.state.inputCP)
+
+
+  getPlaylistTracks = (idDaPlaylist) => {
+    axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${idDaPlaylist}/tracks`,
+    {
+      headers: {
+        Authorization: 'tiago-hennig-turmaGuimaraes'
+      }
+    }).then((response) => {
+      this.setState({faixasDaPlaylist: response.data.result.list})
+      console.log(response.data.result.list)
+    }).catch((error) => console.log(error))
   }
+
+
+
+
+
+
 
   createPlaylist = () => {
     const body = {
@@ -136,8 +80,11 @@ class App extends React.Component {
     }).catch((error) => {
       console.log(error.response.data)
     })
-
   }
+
+
+
+
 
   deletePlaylist = (idDaPlaylist) => {
     axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${idDaPlaylist}`,
@@ -151,19 +98,92 @@ class App extends React.Component {
   }
 
 
+
+
+
+  addTrackToPlaylist = () => {
+    const body = {
+      name: this.state.inputTrack,
+      artist: this.state.inputArtist,
+      url: this.state.inputURL
+    }
+
+    axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/:playlistId/tracks', body,
+    {
+      headers: {
+        Authorization: 'tiago-hennig-turmaGuimaraes'
+      }
+    }).then((response) => {
+      console.log(response.data)
+    }).catch((error) => {
+      console.log(error.response.data)
+    })
+  }
+
+
+
+  handleInputCP = (event) => {
+    this.setState({ inputCP: event.target.value})
+    console.log(this.state.inputCP)
+  }
+
+  handleInputPlaylist = (event) => {
+    this.setState({inputPlaylist: event.target.value})
+    console.log(this.state.inputPlaylist)
+  }
+
+  handleInputTrack = (event) => {
+    this.setState({inputTrack: event.target.value})
+    console.log(this.state.inputTrack)
+  }
+
+  handleInputArtist = (event) => {
+    this.setState({inputArtist: event.target.value})
+    console.log(this.state.inputArtist)
+  }
+
+  handleInputURL = (event) => {
+    this.setState({inputURL: event.target.value})
+    console.log(this.state.inputURL)
+  }
+
+
+
+
+
     render() {
     
     const renderizaPlaylists = this.state.playlists.map((playlist) => {
       return (
-        <div>
+        <DivPlaylistBotao>
           <p key={playlist.id}>{playlist.name}</p>
-          <button onClick={() => this.deletePlaylist(playlist.id)}>Apagar</button>
-        </div>
+          <BotaoApagar onClick={() => this.deletePlaylist(playlist.id)}>x</BotaoApagar>
+        </DivPlaylistBotao>
       )
     })
 
+    // const renderizaDetalhes = this.state.playlists.map((playlist) => {
+    //   return(
+    //     <div>
+    //       <h4 key={this.getPlaylistTracks(playlist.id)}>{playlist.name}</h4>
+    //       {this.state.faixasDaPlaylist.map(item => {
+    //   return(
+    //     <p key={item.id}>{item.name}</p>
+    //   )
+    // })}
+    // </div>
+    //   )
+    // }) 
+
+    // const renderizaFaixas = this.state.faixasDaPlaylist.map(playlist => {
+    //   return(
+    //     <p key={playlist.id}>{playlist.name}</p>
+    //   )
+    // })
+
       return(
-    <div>
+        <Div>
+    <DivPage>
       <Header>
         <p>Labefy</p>
       </Header>
@@ -175,7 +195,7 @@ class App extends React.Component {
             <p>Criar Playlist</p>
 
             <div>
-              <input placeholder='Digite o nome da Playlist' value={this.state.inputCP} 
+              <input placeholder='Nome da nova Playlist' value={this.state.inputCP} 
               onChange={this.handleInputCP}></input>
               <button onClick={this.createPlaylist}>Criar Playlist</button>
             </div>
@@ -185,21 +205,39 @@ class App extends React.Component {
             <p>Lista de Playlists</p>
 
             <div>
-              {renderizaPlaylists}
+              {renderizaPlaylists}              
             </div>
+
           </ListaDePlaylists>
         </Coluna>
 
         <AdicionarMusicas>
           <p>Adicionar Músicas</p>
+
+          <div>
+            <input placeholder='Playlist' value={this.state.inputPlaylist} 
+            onChange={this.handleInputPlaylist}></input>
+            <input placeholder='Nome da música' value={this.state.inputTrack} 
+            onChange={this.handleInputTrack}></input>
+            <input placeholder='Artista' value={this.state.inputArtist} 
+            onChange={this.handleInputArtist}></input>
+            <input placeholder='URL da música' value={this.state.inputURL} 
+            onChange={this.handleInputURL}></input>
+            <button onClick={this.addTrackToPlaylist}>Adicionar música</button>
+          </div>
         </AdicionarMusicas>
 
         <DetalheDaPlaylist>
           <p>Detalhe da Playlist</p>
+
+          <div>
+            {/* {renderizaDetalhes} */}
+          </div>
         </DetalheDaPlaylist>
       </Main>
 
-    </div>
+    </DivPage>
+    </Div>
   );
 }
 }
---
+++ modulo3/projeto-labefy/labefy/src/components/style.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+export const DivPage = styled.div`
+width: 750px;
+align-content:center;
+align-self: center;
+display: flex;
+flex-direction: column;
+`
+
+export const Div = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+
+`
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  height: 90px;
+  background-color: skyblue;
+  font-size: xx-large;
+  font-weight: bold;
+  margin: 10px;
+  border-radius: 10px;
+
+  p {
+    align-self: center;
+  }
+`
+
+export const Main = styled.div`
+display: flex;
+background-color: white;
+`
+
+export const Coluna = styled.div`
+display: flex;
+flex-direction: column;
+width: 300px;
+background-color: white;
+`
+
+export const CriarPlaylist = styled.div`
+display: flex;
+flex-direction: column;
+margin: 10px;
+background-color: aliceblue;
+text-align: center;
+min-height: 100px;
+border-radius: 10px;
+padding-bottom: 20px;
+
+  p {
+    font-weight: bold;
+    font-size: larger;
+  }
+
+  div {
+    display: flex;
+  flex-direction: column;
+  width: 160px;
+  place-self: center;
+  }
+
+  div > input {
+    margin: 3px;
+  }
+
+  div > button {
+    margin: 3px;
+  }
+`
+
+export const ListaDePlaylists = styled.div`
+display: flex;
+flex-direction: column;
+margin: 10px;
+background-color: aliceblue;
+text-align: center;
+min-height: 250px;
+border-radius: 10px;
+
+p {
+  font-size: larger;
+  font-weight: bold;
+}
+`
+
+export const AdicionarMusicas = styled.div`
+display: flex;
+flex-direction: column;
+margin: 10px;
+background-color: aliceblue;
+text-align: center;
+height: 280px;
+border-radius: 10px;
+width: 300px;
+
+p {
+  font-size: larger;
+  font-weight: bold;
+}
+
+div {
+  display: flex;
+  flex-direction: column;
+  width: 180px;
+  align-items: center;
+  align-self: center;
+}
+
+div > button {
+  width: 80px;
+  margin: 10px;
+}
+
+div > input {
+  margin: 5px;
+}
+`
+
+export const DetalheDaPlaylist = styled.div`
+display: flex;
+flex-direction: column;
+margin: 10px;
+background-color: aliceblue;
+text-align: center;
+min-height: 250px;
+border-radius: 10px;
+width: 300px;
+
+  p {
+    font-size: larger;
+    font-weight: bold;
+  }
+`
+
+export const DivPlaylistBotao = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+
+p {
+  padding-right: 3px;
+  font-size: medium;
+  font-weight: 300;
+}
+`
+
+export const BotaoApagar = styled.button`
+color: red;
+align-self: center;
+`
