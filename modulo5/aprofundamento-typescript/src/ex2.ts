f070178ff2b4f74b3f69d50ad0501955d6ef693f
Tiago Hennig <tiago.hennig@hotmail.com>
2022-05-27 14:54:09 -0300

    .

--- modulo5/aprofundamento-typescript/src/ex2.ts
+++ modulo5/aprofundamento-typescript/src/ex2.ts
@@ -2,26 +2,32 @@
 
 // a)
 
-// function obterEstatisticas(numeros:number[]):string[]  | number | any {
+function obterEstatisticas(numeros:number[]):string[]  | number | any {
 
-//     const numerosOrdenados:number[] = numeros.sort(
-//         (a:any, b:any):number => a - b
-//     )
+    const numerosOrdenados:number[] = numeros.sort(
+        (a:any, b:any):number => a - b
+    )
 
-//     let soma:number = 0
+    let soma:number = 0
 
-//     for (let num of numeros) {
-//         soma += num
-//     }
+    for (let num of numeros) {
+        soma += num
+    }
 
-//     const estatisticas = {
-//         maior: numerosOrdenados[numeros.length - 1],
-//         menor: numerosOrdenados[0],
-//         media: soma / numeros.length
-//     }
+    type Estatistica = {
+        maior: number,
+        menor: number,
+        media: number
+    }
 
-//     return estatisticas
-// }
+    const estatisticas: Estatistica = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
 
 // console.log(obterEstatisticas([10,20,30,40,50]))
 
@@ -32,16 +38,23 @@
 
 // c)
 
-// type amostraDeIdades = {
+type Estatistica = {
+    maior: number,
+    menor: number,
+    media: number
+}
 
-//     numeros: number[],
-//     obterEstatisticas: (numeros:number[]) => object
+type amostraDeIdades = {
+
+    numeros: number[],
+    obterEstatisticas: (numeros:number[]) => Estatistica
     
-// };
+};
 
-// const jogos:amostraDeIdades = {
-//     numeros: [10,20,30],
-//     obterEstatisticas: ([10,20,30]) => 
+const jogos:amostraDeIdades = {
+    numeros: [10,20,30],
+    obterEstatisticas
+}
 
 // FALTA CONCLUIR O C)
 
