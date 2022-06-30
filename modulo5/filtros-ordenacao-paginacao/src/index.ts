ed8395b0ec96304f5bd6aaf8ab81e78161d9db55
Tiago Hennig <tiago.hennig@hotmail.com>
2022-06-29 22:04:49 -0300

    1 ok

--- modulo5/filtros-ordenacao-paginacao/src/index.ts
+++ modulo5/filtros-ordenacao-paginacao/src/index.ts
@@ -2,8 +2,80 @@ import connection from "./connection";
 import app from "./app";
 import { Request, Response } from "express"
 
+// 1
+
+// a
+
+// app.get("/users/", async (req: Request, res: Response) => {
+
+//     let name = req.query.name
+
+//     if (!name) {
+//         name = ""
+//     }
+
+//     try {
+//         const result = await connection.raw(`
+//     SELECT id, name, email, type
+//     FROM aula48_exercicio WHERE name LIKE '%${name}%'
+//     `)
+
+//     res.status(200).send(result[0])
+//     } catch (err) {
+//         res.send(err)
+//     }
 
 
+// })
+
+// b
+
+// app.get("/users/:type", async (req: Request, res: Response) => {
+
+//     let type = req.params.type
+
+//     if (!type) {
+//         type = ""
+//     }
+
+//     try {
+//         const result = await connection.raw(`
+//     SELECT id, name, email, type
+//     FROM aula48_exercicio WHERE type LIKE '%${type}%'
+//     `)
+
+//     res.status(200).send(result[0])
+//     } catch (err) {
+//         res.send(err)
+//     }
+    
+// })
+
+// 2
+
+// a
+
+app.get("/users/:type", async (req: Request, res: Response) => {
+
+    let type = req.params.type
+
+    if (!type) {
+        type = ""
+    }
+
+    try {
+        const result = await connection.raw(`
+    SELECT id, name, email, type
+    FROM aula48_exercicio WHERE type LIKE '%${type}%'
+    ORDER BY email ASC
+    `)
+
+    res.status(200).send(result[0])
+    } catch (err) {
+        res.send(err)
+    }
+    
+})
 
 
 
