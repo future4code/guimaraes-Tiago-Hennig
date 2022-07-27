23253dbad5a6ece3dfc32f8c3df83cda4663c818
Tiago Hennig <tiago.hennig@hotmail.com>
2022-07-27 16:54:02 -0300

    .

---
+++ modulo5/heranca-polimorfismo/src/classes/User.ts
@@ -0,0 +1,32 @@
+
+
+export class User {
+
+    constructor(
+        private id: string,
+        private email: string,
+        private name: string,
+        private password: string
+    ) {
+        console.log("Chamando o construtor da classe User")
+    }
+
+    public getId(): string {
+        return this.id
+    }
+
+    public getEmail(): string {
+        return this.email
+    }
+
+    public getName(): string {
+        return this.name
+    }
+
+    public introduceYourself(): string {
+        return `Olá! Sou ${this.name}! Bom dia!`
+    }
+}
+
+
+
