a111588fdef718175d148a424602e2c2ec9a3761
Tiago Hennig <tiago.hennig@hotmail.com>
2022-08-11 19:16:23 -0300

    conclusao

--- modulo6/projeto-labook/src/controller/UserController.ts
+++ modulo6/projeto-labook/src/controller/UserController.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 import { UserBusiness } from "../business/UserBusiness";
 import { UserInputDTO } from "../model/user";
 
